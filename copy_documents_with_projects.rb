4ae3d93665c28bf802e3249f18f74d42810de5ee
Nathan Stitt <nathan@stitt.org>
2014-07-16 11:13:55 -0500

    Variable names were incorrect
    The script originated out of an IRB session and some of the vars still referred to the shorter varients

--- copy_documents_with_projects.rb
+++ copy_documents_with_projects.rb
@@ -1,17 +1,17 @@
-new_account = Account.find(XX)
-old_account = Account.find(XX)
+new_account = Account.find(8858)
+old_account = Account.find(3380)
 
 Document.duplicate(old_account.document_ids, new_account)
 
 project_map = old_account.projects.each_with_object({}){ |project, map|
-  map[project] = Project.create(prj.attributes.except('id').merge({'account_id'=>new_account.id}))
+  map[project] = Project.create(project.attributes.except('id').merge({'account_id'=>new_account.id}))
 }
 
 # wait for duplication job to finish
 
 # map the old document's to the new ones
 documents_map = new_account.documents.each_with_object({}){|new_doc, map|
-  if (old_doc = old_account.documents.where(title:d.title, description:d.description).first)
+  if (old_doc = old_account.documents.where(title:new_doc.title, description:new_doc.description).first)
     map[new_doc] = old_doc
   end
 }
